62e262af77447bb42536ac1e401d6259e4d88dd0
Alex Riegler <53669754+alexriegler@users.noreply.github.com>
2023-01-31 02:40:48 -0600

    Chapter: Rendering Sprites

--- src/resource_manager.h
+++ src/resource_manager.h
@@ -31,16 +31,16 @@ public:
   // loads (and generates) a shader program from file loading vertex, fragment
   // (and geometry) shader's source code. If gShaderFile is not nullptr, it also
   // loads a geometry shader
-  static Shader LoadShader(const char* vShaderFile,
-                           const char* fShaderFile,
-                           const char* gShaderFile,
-                           std::string name);
+  static Shader& LoadShader(const char* vShaderFile,
+                            const char* fShaderFile,
+                            const char* gShaderFile,
+                            std::string name);
   // retrieves a stored shader
-  static Shader GetShader(std::string name);
+  static Shader& GetShader(std::string name);
   // loads (and generates) a texture from file
-  static Texture2D LoadTexture(const char* file, bool alpha, std::string name);
+  static Texture2D& LoadTexture(const char* file, bool alpha, std::string name);
   // retrieves a stored texture
-  static Texture2D GetTexture(std::string name);
+  static Texture2D& GetTexture(std::string name);
   // properly de-allocates all loaded resources
   static void Clear();
 
@@ -49,11 +49,11 @@ private:
   // objects. Its members and functions should be publicly available (static).
   ResourceManager() {}
   // loads and generates a shader from file
-  static Shader loadShaderFromFile(const char* vShaderFile,
-                                   const char* fShaderFile,
-                                   const char* gShaderFile = nullptr);
+  static Shader& loadShaderFromFile(const char* vShaderFile,
+                                    const char* fShaderFile,
+                                    const char* gShaderFile = nullptr);
   // loads a single texture from file
-  static Texture2D loadTextureFromFile(const char* file, bool alpha);
+  static Texture2D& loadTextureFromFile(const char* file, bool alpha);
 };
 
 #endif

e2310f118e5d1b0db6101e300ec08d253213a268
Alex Riegler <53669754+alexriegler@users.noreply.github.com>
2023-02-25 18:36:32 -0600

    Chapter: Levels

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,3 @@
-
-
 /*******************************************************************
 ** This code is part of Breakout.
 **
@@ -9,11 +7,14 @@
 ** option) any later version.
 ******************************************************************/
 #include "game.h"
+#include "game_object.h"
 #include "resource_manager.h"
 #include "sprite_renderer.h"
 
 // Game-related State data
+// TODO: Use smart pointers
 SpriteRenderer* Renderer;
+GameObject* Player;
 
 Game::Game(unsigned int width, unsigned int height)
     : State(GAME_ACTIVE)
@@ -26,6 +27,7 @@ Game::Game(unsigned int width, unsigned int height)
 Game::~Game()
 {
   delete Renderer;
+  delete Player;
 }
 
 void Game::Init()
@@ -45,18 +47,66 @@ void Game::Init()
   // set render-specific controls
   Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
   // load textures
+  // TODO: Set a default directory for textures, etc.
+  // TODO: Default name of a texture should the file's name without the
+  //       extension
+  ResourceManager::LoadTexture("textures/background.jpg", false, "background");
   ResourceManager::LoadTexture("textures/awesomeface.png", true, "face");
+  ResourceManager::LoadTexture("textures/block.png", false, "block");
+  ResourceManager::LoadTexture(
+      "textures/block_solid.png", false, "block_solid");
+  ResourceManager::LoadTexture("textures/paddle.png", true, "paddle");
+  // load levels
+  // TODO: Improve level loading
+  GameLevel one;
+  one.Load("levels/one.lvl", this->Width, this->Height / 2);
+  GameLevel two;
+  two.Load("levels/two.lvl", this->Width, this->Height / 2);
+  GameLevel three;
+  three.Load("levels/three.lvl", this->Width, this->Height / 2);
+  GameLevel four;
+  four.Load("levels/four.lvl", this->Width, this->Height / 2);
+  this->Levels.push_back(one);
+  this->Levels.push_back(two);
+  this->Levels.push_back(three);
+  this->Levels.push_back(four);
+  this->Level = 0;
+  // configure game objects
+  glm::vec2 playerPos = glm::vec2(this->Width / 2.0f - PLAYER_SIZE.x / 2.0f,
+                                  this->Height - PLAYER_SIZE.y);
+  Player = new GameObject(
+      playerPos, PLAYER_SIZE, ResourceManager::GetTexture("paddle"));
 }
 
 void Game::Update(float dt) {}
 
-void Game::ProcessInput(float dt) {}
+void Game::ProcessInput(float dt)
+{
+  if (this->State == GAME_ACTIVE) {
+    float velocity = PLAYER_VELOCITY * dt;
+    // move playerboard
+    if (this->Keys[GLFW_KEY_A]) {
+      if (Player->Position.x >= 0.0f)
+        Player->Position.x -= velocity;
+    }
+    if (this->Keys[GLFW_KEY_D]) {
+      if (Player->Position.x <= this->Width - Player->Size.x)
+        Player->Position.x += velocity;
+    }
+  }
+}
 
 void Game::Render()
 {
-  Renderer->DrawSprite(ResourceManager::GetTexture("face"),
-                       glm::vec2(200.0f, 200.0f),
-                       glm::vec2(300.0f, 400.0f),
-                       45.0f,
-                       glm::vec3(0.0f, 1.0f, 0.0f));
+  if (this->State == GAME_ACTIVE) {
+    // draw background
+    Renderer->DrawSprite(ResourceManager::GetTexture("background"),
+                         glm::vec2(0.0f, 0.0f),
+                         glm::vec2(this->Width, this->Height),
+                         0.0f);
+    // draw level
+    this->Levels[this->Level].Draw(*Renderer);
+    // draw player
+    Player->Draw(*Renderer);
+  }
 }
--- src/game.h
+++ src/game.h
@@ -9,6 +9,8 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "game_level.h"
+
 // clang-format off
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -22,6 +24,13 @@ enum GameState
   GAME_WIN
 };
 
+// Initial size of the player paddle
+// TODO: Change to lowercase
+// TODO: Use constexpr/constinit
+const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
+// Initial velocity of the player paddle
+const float PLAYER_VELOCITY(500.0f);
+
 // Game holds all game-related state and functionality.
 // Combines all game-related data into a single class for
 // easy access to each of the components and manageability.
@@ -32,6 +41,8 @@ public:
   GameState State;
   bool Keys[1024];
   unsigned int Width, Height;
+  std::vector<GameLevel> Levels;
+  unsigned int Level;
   // constructor/destructor
   Game(unsigned int width, unsigned int height);
   ~Game();
